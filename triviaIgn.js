288a9438eb869ea8454cc76b4f304ab542171d84
Christian Gastrell <cg@ubuntu-vm.(none)>
2014-02-13 15:27:03 -0300

    rev statusBar

--- triviaIgn.js
+++ triviaIgn.js
@@ -6,7 +6,10 @@
 		},
 		_create: function(){
 			this.element.html("");
-			this.statusBar = $('<div />')
+			this.buildStatusBar();
+		},
+		buildStatusBar: function() {
+			this.statusBar = $('<div id="statusBar" />')
 				.css({
 					"background": "black",
 					color: "white"
@@ -15,15 +18,36 @@
 			this.hiScoreLabel = $('<span />')
 				.text("Record del día: ")
 				.css({
-					display: "block",
-					width: "30%"
+					display: "inline-block",
+					width: "30%",
+					'margin-left': '2%'
 				})
 				.appendTo(this.statusBar);
-
 			this.hiScoreValue = $('<span />')
 				.text(this.options.hiScore)
-				.css('color','yellow')
 				.appendTo(this.hiScoreLabel);
+
+			this.gameTitle = $('<span />')
+				.text("Geo Trivia")
+				.css({
+					display: 'inline-block',
+					width: '36%',
+					'text-align': 'center'
+				}).appendTo(this.statusBar);
+
+			this.scoreLabel = $('<span />')
+				.text('Puntaje: ')
+				.css({
+					display: 'inline-block',
+					width: '30%',
+					'text-align': 'right',
+					'margin-right': '2%'
+				}).appendTo(this.statusBar);
+			this.scoreValue = $('<span />')
+				.text("0")
+				.css({
+					color: 'yellow'
+				}).appendTo(this.scoreLabel);
 				
 			this.statusBar.appendTo(this.element);
 		}
